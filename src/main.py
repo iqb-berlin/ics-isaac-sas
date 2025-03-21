3856552ad361890a8c80bcce634d20512a0cc63a
paflov <philipp.franck@hu-berlin.de>
2025-03-21 10:43:22 +0100

    Return correctly shaped Erorrs

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 import isaac_sas.core
-
 
 from apis.default_api import router as DefaultApiRouter
 from apis.native_api import router as NativeApiRouter
@@ -16,16 +17,28 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"]
+    allow_origins = ['*'],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+    expose_headers = ["*"]
 )
 
 app.include_router(DefaultApiRouter)
 app.include_router(NativeApiRouter)
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    error_text = ''
+    for error in exc.errors():
+        error_text += error["msg"] + ': ' + error["loc"][-1] + '\n'
 
+    return JSONResponse(
+        status_code = 422,
+        content = {
+            "type": 'invalid-data',
+            "message": error_text
+        }
+    )
 
 
