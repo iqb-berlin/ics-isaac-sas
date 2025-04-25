ebbc86eb4dc178cfee2ca10891493e10b8c7a44b
paflov <philipp.franck@hu-berlin.de>
2025-04-25 16:43:31 +0200

    Return real info at /info endpoint instead of dummy data

--- ics/ics/implementation.py
+++ ics/ics/implementation.py
@@ -10,6 +10,10 @@ class TaskInstructions(BaseModel):
     item_prompt: StrictStr = Field(description="the prompt string (question) the answer was given in response for", alias="itemPrompt")
     item_targets: List[StrictStr] = Field(description="a list of correct reference answers for the current item", alias="itemTargets")
     random_seed: StrictInt|None = Field(default=None, description="Leave out for true randomness or enter a id for replicable results", alias="randomSeed")
+    @staticmethod
+    def description() -> str:
+        return "Isaac-SaS also uses a list of possible correct answers from the code book for training along with teh encoded training data."
+
 
 class Task(TaskBase):
     instructions: Optional[TaskInstructions] = None
